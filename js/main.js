3015fb50d162b322644fc5d9a6e153b3549fdcf2
Mike Eng <mrengy@gmail.com>
2017-11-19 14:54:59 -0500

    revealing letter matching that clicked

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $( document ).ready(function() {
-    
+  function revealLetter(){
+    var thistxt = $(this).text();
+    $('.item-letter:contains('+thistxt+')').parent().addClass('completed');
+  }
+
+  $('.letter').on('click', revealLetter);
 });
