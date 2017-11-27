e4285c72597e24deaebdba95d326c646d971fcc4
Mike Eng <mrengy@gmail.com>
2017-11-26 19:16:47 -0500

    dynamically writing message

--- js/main.js
+++ js/main.js
@@ -4,5 +4,29 @@ $( document ).ready(function() {
     $('.item-letter:contains('+thistxt+')').parent().addClass('completed');
   }
 
+  //constructor for target objects
+  function Object(type, name){
+    this.type = type;
+    this.name = name;
+    this.added = false;
+  }
+
+  var message = "HAPPY HOLIDAYS";
+  var messageHTML = '';
+
+  function writeMessage(){
+    for (var i=0, len=message.length; i<len; i++){
+      //messageHTML+= 'a';
+
+      if (message.charAt(i) == ' '){
+        messageHTML += '<div class=\"item space\"><div class=\"item-letter\">&nbsp;<\/div><\/div>';
+      }
+      else {
+        messageHTML += '<div class=\"item\"><div class=\"item-letter\">'+messageHTML.charAt(i)+'<\/div><\/div>';
+      }
+    }
+  }
+  writeMessage();
+  console.log(messageHTML);
   $('.letter').on('click', revealLetter);
 });
