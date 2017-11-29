e2eff27deb3f7d7c1b4392f7f98386eff0936448
Mike Eng <mrengy@gmail.com>
2017-11-28 20:56:47 -0500

    checkpoint in creating letter objects - repeated H bug

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $( document ).ready(function() {
+  var message = "HAPPY HOLIDAYS";
+  var messageHTML = '';
+  var objects = new Array();
+  var alreadyExists = false;
+
   function revealLetter(){
     var thistxt = $(this).text();
     $('.item-letter:contains('+thistxt+')').parent().addClass('completed');
@@ -8,11 +13,8 @@ $( document ).ready(function() {
   function Object(type, name){
     this.type = type;
     this.name = name;
-    this.added = false;
+    this.completed = false;
   }
-
-  var message = "HAPPY HOLIDAYS";
-  var messageHTML = '';
 
   function writeMessage(){
     for (var i=0, len=message.length; i<len; i++){
@@ -21,7 +23,28 @@ $( document ).ready(function() {
       }
       else {
         messageHTML += '<div class=\"item\"><div class=\"item-letter\">'+message.charAt(i)+'<\/div><\/div>';
-        //create objects here
+
+        //create objects for letters that don't yet exist
+        //if ($.inArray(message.charAt(i),objects) == -1){
+        for (var j=0, length=objects.length; j<len; j++){
+          //if array has an item
+          if (objects[j]!= undefined){
+            //detect whether the object of this letter has been created already or it's a space
+            if ( (objects[j].name == message.charAt(i) ) || (message.charAt(i) != ' ') ){
+              console.log('letter found');
+              alreadyExists = true;
+            }
+          }
+
+          // add the letter if it doesn't already exist - LOOPING TOO MANY TIMES, PROBABLY DUE TO OBJECTS.LENGTH BEING RE-EVALUATED WITH EACH PUSH
+          if (j >= (length - 1) && alreadyExists == false ){
+            console.log('letter object created');
+            objects.push(new Object('letter',message.charAt(i)));
+          }
+
+          //reset
+          alreadyExists = false;
+        }
       }
 
       if (i == (len - 1)){
@@ -31,4 +54,8 @@ $( document ).ready(function() {
   }
   writeMessage();
   $('.letter').on('click', revealLetter);
+
+  console.log(objects);
+  console.log(objects.length);
+
 });
