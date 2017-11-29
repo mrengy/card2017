a1885b9f8af81ccd899fd1dde5a83a039de02754
Mike Eng <mrengy@gmail.com>
2017-11-29 08:30:41 -0500

    successfully creating letter objects

--- js/main.js
+++ js/main.js
@@ -34,28 +34,23 @@ $( document ).ready(function() {
         }
         //create objects for letters that don't yet exist
         //iterate through objects that have been created
-        //if ($.inArray(message.charAt(i),objects) == -1){
         for (var j=0, length=objectsLength; j<length; j++){
           //if array has an item
           if (objects[0]!= undefined){
-            //detect whether the object of this letter has been created already or it's a space
-            //NOT LOOPING ENOUGH TO CATCH A DUPLICATE H. Try https://stackoverflow.com/questions/26943242/remove-duplicate-objects-in-an-array-of-object-in-javascript
-            if ( (objects[j].name == message.charAt(i) ) || (message.charAt(i) == ' ') ){
-              console.log('letter found');
+            //detect whether the object of this letter has been created already
+            if ( objects[j].name == message.charAt(i) ){
+              //console.log('letter found');
               alreadyExists = true;
             }
           }
-
           // add the letter if it doesn't already exist
           if (j >= (length - 1) && alreadyExists == false ){
-            console.log('letter object created');
+            //console.log('letter object created');
             objects.push(new Object('letter',message.charAt(i)));
           }
-
-          //reset
-          alreadyExists = false;
         }
-
+        //reset
+        alreadyExists = false;
       }
 
       if (i == (len - 1)){
@@ -65,8 +60,4 @@ $( document ).ready(function() {
   }
   writeMessage();
   $('.letter').on('click', revealLetter);
-
-  console.log(objects);
-  console.log(objects.length);
-
 });
